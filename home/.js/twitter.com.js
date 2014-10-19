291baa5d73cf08db1ae9e9391f3aa24375506eaa
Denis Laliberté <denis@tp1.ca>
2014-10-19 16:16:31 -0400

    remove retweet

--- home/.js/twitter.com.js
+++ home/.js/twitter.com.js
@@ -1,5 +1,7 @@
 //no sidebar junk twitter
 $('.dashboard-left').hide()
 $('.dashboard-right').hide()
-$('#timeline').width('620px');
-$('.content-main').css('float','left');
+$('#timeline').width('620px')
+$('.content-main').css('float','left')
+//remove retweet, only on the first load of the page
+$('[data-retweeter]:not([data-retweeter=""])').hide()
