8ef6dc6de1cdec4c8a08518460706f86b4ec7a16
Denis Laliberté <denis@tp1.ca>
2014-10-19 12:15:05 -0400

    add twitter.js

---
+++ home/.js/readme.md
@@ -0,0 +1,5 @@
+# .js
+
+Javascript file to be execute on specific website
+
+See https://github.com/defunkt/dotjs
---
+++ home/.js/twitter.com.js
@@ -0,0 +1,5 @@
+//no junk // full screen twitter
+$('.dashboard-left').hide()
+$('.dashboard-right').hide()
+$('#timeline').width('620px');
+$('.content-main').css('float','left');
