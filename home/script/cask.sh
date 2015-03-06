c21be2e4e9eed3b13fd8f8221df52b0b9e236cf6
Denis Laliberté <denis@tp1.ca>
2015-03-06 14:48:42 -0500

    add cask script

---
+++ home/script/cask.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+brew cask install firefox
+#brew cask install chrome
+#brew cask install iterm
+brew cask install alfred
+brew cask install atom
+#brew cask install sequel-pro
+#brew cask install pgadmin3
