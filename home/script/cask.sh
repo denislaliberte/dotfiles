dbf501563460a1574d3f30c312e563370ec0f883
Denis Laliberté <denis@tp1.ca>
2015-03-06 15:17:39 -0500

    udpate script

--- home/script/cask.sh
+++ home/script/cask.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 brew cask install firefox
+#brew cask install thunderbird
 #brew cask install chrome
 #brew cask install iterm
 brew cask install alfred
 brew cask install atom
-#brew cask install sequel-pro
+brew cask install sequel-pro
 #brew cask install pgadmin3
