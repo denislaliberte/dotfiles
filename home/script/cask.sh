79f6fb77e20c32fc35a1e5322afc03d3ca8b40ae
Denis Laliberté <denis@tp1.ca>
2015-03-16 12:21:20 -0400

    add cask

--- home/script/cask.sh
+++ home/script/cask.sh
@@ -7,4 +7,6 @@ brew cask install firefox
 brew cask install alfred
 brew cask install atom
 brew cask install sequel-pro
+brew cask install postgres
+#brew cask install jing
 #brew cask install pgadmin3
