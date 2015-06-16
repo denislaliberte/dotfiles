9196c9cf76d828a986662ba09bfa7bc53e862eae
Denis Laliberté <denis@tp1.ca>
2015-06-16 18:03:21 -0400

    no comments

--- home/hc/alias/mysql.sh
+++ home/hc/alias/mysql.sh
@@ -1,10 +1,10 @@
 #mysql use "export DATAB=database_name && src"
 alias my="mysql"
-alias myr="mysql -uroot -h127.0.0.1"
-alias mye="mysql -uroot -h127.0.0.1 $DATAB -e "
+alias myr="mysql"
+alias mye="mysql $DATAB -e "
 alias mysql="echo '#### use my | myr | mye ####' && mysql";
-alias mysdg="mysql -uroot -h127.0.0.1 -e 'show databases;' |grep"
-alias mysd="mysql -uroot -h127.0.0.1 -e 'show databases;'"
-alias myst="mysql -uroot -h127.0.0.1 -e 'show tables;' $DATAB"
-function mydes(){ mysql -uroot -h127.0.0.1 -e "desc $1;" $DATAB}
-function mysel(){ mysql -uroot -h127.0.0.1 -e "select * from $1 limit 10;" $DATAB}
+alias mysdg="mysql -e 'show databases;' |grep"
+alias mysd="mysql -e 'show databases;'"
+alias myst="mysql -e 'show tables;' $DATAB"
+function mydes(){ mysql -e "desc $1;" $DATAB}
+function mysel(){ mysql -e "select * from $1 limit 10;" $DATAB}
