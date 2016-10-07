6625cb58f9a78bb1bf283b67d7a99c8ad3147979
Denis Laliberte <denis.laliberte@gmail.com>
2016-10-07 16:25:00 -0400

    no comment

--- home/alias/path.sh
+++ home/alias/path.sh
@@ -35,4 +35,4 @@ alias tp=tee_path
 function tee_path() { tee -a "path${2:-1}.ignore" }
 function next_path() { i=1; while [[ -e "$1$i.ignore" ]] ; do let i++; done ; echo "$1$i.ignore"  }
 alias mp=merge_path
-function merge_path() { cat path*.ignore | sort | uniq > temp.ignore; rm path*ignore; mv temp.ignore path1.ignore; select_path }
+function merge_path() { cat path*.ignore | LC_ALL=C  sort | uniq > temp.ignore; rm path*ignore; mv temp.ignore path1.ignore; select_path }
