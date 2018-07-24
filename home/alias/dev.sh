4ca93140a51465489d544d1232339776767a2900
Denis Laliberte <denis.laliberte@gmail.com>
2018-07-24 14:19:32 -0400

    Refactor multiline alias to functions

--- home/alias/dev.sh
+++ home/alias/dev.sh
@@ -1,32 +1,7 @@
-function dddu(){ dev sv stop; dev down; nvm use 8.9.4; dev up; dev s}
-
-function trash_railgun() { railgun rm-data;  dev reset-railgun; }
-
-function trash_image_magick(){ brew remove --force $(brew list | grep 'imagemagick\|v8\|librsvg') --ignore-dependecies; }
-
-function trash_image_gem() { gem uninstall $(gem list | grep 'image\|magick\|sassc') }
-
-function trash_image() { trash_image_magick; trash_image_gem}
-
-function reborn() { dev sv stop; dev down;  trash_railgun; nvm use 8.9.4; trash_image }
-
-function devclone() {dev clone Shopify/$1; git checkout origin/master; git branch -D master; linkignore $1 }
-
-function linkignore(){  ln -s $HOME/src/github.com/Shopify/.ignore $HOME/src/github.com/Shopify/$1/.ignore}
-
-function fullreborn() { dev cd ${1:-shopify}; reborn; rm -rf **/node_modules && yarn cache clean; rm -rf ~/.gem; cd $HOME/src/github.com/Shopify; mv ${1:-shopify} ${1:-shopify}-$(date '+%Y%m%d-%H%M-%s'); devclone ${1:-shopify}; sudo shutdown -r now; }
-
-function devclone(){ dev clone Shopify/${1:-shopify}; git checkout origin/master; git branch -D master}
 alias dcs="dev cd shopify"
 alias dcp="dev cd partners"
 alias dci="dev cd identity"
-alias u="dev up" # old dup # Install and start everything necessary for an app to run.
-
+alias u="dev up"
 alias dt="deprecated spt"
-
 alias dta="dev test"
-function dev_server(){ dev open internal; dev server}
-alias ds=dev_server
 alias opn="dev open"
-function dev_style(){ dev style --include-branch-commits }
-alias stl=dev_style
