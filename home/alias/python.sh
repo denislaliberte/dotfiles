4903ff5d665242b6c33e57bc5e82d5e41b39990f
Denis Laliberte <denis.laliberte@gmail.com>
2019-01-28 08:52:15 -0500

    no comment

--- home/alias/python.sh
+++
@@ -1,8 +0,0 @@
-alias py="python"
-alias pc="python -c" # execute python command
-alias fl="fab list"
-alias pys="python -m SimpleHTTPServer 8000"
-alias pyw="pywatch"
-alias pjf=python_json_format
-function python_json_format() { echo $1 | python -m json.tool }
-
